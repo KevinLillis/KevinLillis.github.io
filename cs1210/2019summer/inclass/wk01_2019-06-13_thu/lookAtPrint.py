0e6c81e88b9b25b94a91574b5dc3b011675e4074
Kevin Lillis <kmlillis30@hotmail.com>
2019-06-13 11:39:12 -0500

    1210 added Thursday's in-class files

---
+++ cs1210/2019summer/inclass/wk01_2019-06-13_thu/lookAtPrint.py
@@ -0,0 +1,10 @@
+# lookAtPrint.py
+
+def main():
+    message = 'zzz'
+    print(message, end = '<>')
+    print(message, end = 'XYZ')
+    print(message, end = '')
+    print(message, end = '\n')
+    
+main()
