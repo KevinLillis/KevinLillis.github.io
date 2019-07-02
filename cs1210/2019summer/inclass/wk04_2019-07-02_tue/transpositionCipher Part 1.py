5607ae19f36836f38f3edc8851b3806bf8cc8e87
Kevin Lillis <kmlillis30@hotmail.com>
2019-07-02 12:17:07 -0500

    1210, added transposition cipher

--- cs1210/2019summer/inclass/wk04_2019-07-02_tue/transpositionCipher Part 1.py
+++ cs1210/2019summer/inclass/wk04_2019-07-02_tue/transpositionCipher Part 1.py
@@ -1,7 +1,7 @@
 # First Part of Transposition Cipher
 # Only includes the encryption part
 
-def scramble2Encrypt(plainText):
+def scramble2Encrypt1(plainText):
     ''' This is how our book does it.'''
     oddChars = ''
     evenChars = ''
@@ -20,11 +20,43 @@ def scramble2Encrypt(plainText):
     cipherText = oddChars + evenChars
     return cipherText
 
+
+def scramble2Encrypt2(plainText):
+    ''' This is a tweak of how our book does it.'''
+    oddChars = ''
+    evenChars = ''
+    
+    for index in range(len(plainText)):
+        if index % 2 == 1:
+            # This is an odd index
+            oddChars += plainText[index]
+        else:
+            # This is an even index
+            evenChars += plainText[index]
+            
+    
+    cipherText = oddChars + evenChars
+    return cipherText
+
+
+def scramble2Encrypt3(plainText):
+    ''' This is a more "Pythonic" way to ndo it.'''
+    oddChars = plainText[1::2]
+    evenChars = plainText[::2]
+    
+    cipherText = oddChars + evenChars
+    return cipherText
+
 def main():
     message = input('Enter a message: ')
     print('Original message:', message)
     
-    cipher = scramble2Encrypt(message)
-    print('Encrypted message', cipher)
+    cipher1 = scramble2Encrypt1(message)
+    cipher2 = scramble2Encrypt2(message)
+    cipher3 = scramble2Encrypt3(message)
+    
+    print(cipher1)
+    print(cipher2)
+    print(cipher3)
     
 main()
