4b35ac76faa7d53667e0da0b85546585f185360b
Kevin Lillis <kmlillis30@hotmail.com>
2019-08-02 10:54:45 -0500

    tutoring, daily files and HW 06

---
+++ tutoring/2019summer/sessions/2019-08-01/Dice Simulator.py
@@ -0,0 +1,12 @@
+import random
+
+numRolls = 10000
+
+counts = ['X',0,0,0,0,0,0]
+
+for i in range(numRolls):
+    die = random.randint(1, 6)
+    counts[die] = counts[die] + 1
+    
+for i in range(1, 7):
+    print(f'The value {i} was rolled {counts[i]} times.')
