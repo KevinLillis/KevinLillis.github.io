14d90f55b7254bb2d87917ed1b4304d634a7cae5
Kevin Lillis <kmlillis30@hotmail.com>
2019-10-14 17:37:27 -0500

    295 Daily Files, 630 Update

---
+++ csci295/2019fall/inclass/wk09_2019-10-14_mon/SortedListInterface.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Dr. Lillis
+ */
+public interface SortedListInterface {
+    
+    /**
+     * Returns the first item in the list, or returns null if the list is empty.
+     * @return 
+     */
+    String getFirst();
+    
+    /**
+     * Returns the last item in the list, or returns null if the list is empty.
+     * @return 
+     */
+    String getLast();
+    
+    
+
+}
