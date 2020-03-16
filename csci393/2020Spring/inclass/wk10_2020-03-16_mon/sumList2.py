484fe45621e7948a2994e835657b82df03e0caab
Kevin Lillis <kmlillis30@hotmail.com>
2020-03-16 17:00:15 -0500

    csci 393 Daily files and whiteboard session

---
+++ csci393/2020Spring/inclass/wk10_2020-03-16_mon/sumList2.py
@@ -0,0 +1,23 @@
+# sumList2.py
+# Passes indexes instead of making copies of the smaller lists
+def sumList(L, low = None, high = None):
+    if low == None:
+        # if now equals None, then both low and high equal None
+        low = 0
+        high = len(L) - 1
+        
+    if low > high:
+        # empty list
+        return None
+    
+    if low == high:
+        # list with one element
+        return L[low]
+    
+    middle = (low + high) // 2
+    return sumList(L, low, middle) + sumList(L, middle+1, high)
+
+print(sumList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(sumList(range(101)))
+print(sumList([]))
+print(sumList([4]))
