7ec1249ce67d196929cc569bc19d9b8b7c9a0667
Kevin Lillis <kmlillis30@hotmail.com>
2020-04-17 10:13:11 -0500

    294 Daily Files

---
+++ csci294/2020Spring/inclass/wk14_2020-04-17_fri/Pointers2.cpp
@@ -0,0 +1,38 @@
+// File: Pointers2.cpp
+# include <iostream>
+using namespace std;
+
+int main(){
+	int intVar    = 1234;    // integer variable with a value
+	int * intPtr  = &intVar; // pointer assigned the memory location of intVar
+	int &intRef   = intVar;  // an alias for intVar
+	int *p        = &intRef; // point assinged intRev's memory address
+	
+	cout << " 1) Address of intVar: &intVar = " << &intVar << endl;
+	cout << " 2) Value stored in intVar: intVar = " << intVar << endl;
+	cout << endl;
+	
+	cout << " 3) Address of intPtr: &intPtr = " << &intPtr << endl;
+	cout << " 4) Value stored in intPtr: intPtr = " << intPtr << endl;
+	cout << endl;
+	
+	cout << " 5) Address of intRef: &intRef = " << &intRef << endl;
+	cout << " 6) Value stored in intRef: intRef = " << intRef << endl;
+	cout << endl;
+	
+	cout << " 7) Address of p: &p = " << &p << endl;
+	cout << " 8) Value stored in p: p = " << p << endl;
+	cout << endl;
+
+	cout << " 9) intVar = "  << intVar << endl;
+	cout << "10) *intPtr = " << *intPtr << endl;
+	cout << "11) intRef = "  << intRef << endl;
+	cout << "12) *p = "      << *p << endl;
+	cout << endl;
+
+	cout << "13) &intVar = " << &intVar << endl;
+	cout << "14) intPtr = " << intPtr << endl;
+	cout << "15) &intRef = " << &intRef << endl;
+	cout << "16) p = " << p << endl; 
+	return EXIT_SUCCESS;
+}
---
+++ csci294/2020Spring/inclass/wk14_2020-04-17_fri/Pointers3.cpp
@@ -0,0 +1,33 @@
+// File: Pointers3.cpp
+
+/* Doing "pass by reference" using pointers */
+# include <iostream>
+using namespace std;
+
+int squareByValue(int x);
+int squareByReference(int * x);
+
+int main(){
+	int number = 8;
+	cout << "1) initial value of number = " << number << endl;
+	cout << endl;
+	
+	cout << "2) number squared by value = " << squareByValue(number) << endl;
+	cout << "   number is still equal to: number = " << number << endl;
+	cout << endl;
+	
+	cout << "3) number squared by reference = " << squareByReference(&number) << endl;
+	cout << "   number has been changed: number = " << number << endl;
+
+	return EXIT_SUCCESS;
+}
+
+int squareByValue(int x){
+	x = x * x;
+	return x;
+}
+
+int squareByReference(int * x){
+	*x = *x   *    *x;
+	return *x;
+}
