8d7c5fc28e19f44e742b895501275374448bf56d
Kevin Lillis <kmlillis30@hotmail.com>
2020-04-20 10:36:22 -0500

    294 daily files

---
+++ csci294/2020Spring/inclass/wk15_2020-04-20_mon/Pointers4.cpp
@@ -0,0 +1,24 @@
+// File: Pointers4.cpp
+# include <iostream>
+using namespace std;
+
+void squareAndDouble(int, int *, int *);
+
+int main(){
+	int value{7};
+	int valueSquared{};
+	int valueDoubled{};
+	
+	squareAndDouble(value, &valueSquared, &valueDoubled);
+	
+	cout << "value: " << value << endl;
+	cout << "valueSquared: " << valueSquared << endl;
+	cout << "valueDoubled: " << valueDoubled << endl;
+	
+	return EXIT_SUCCESS;
+}
+
+void squareAndDouble(int x, int *xSquared, int *xDoubled){
+	*xSquared = x * x;
+	*xDoubled = x + x;
+}
