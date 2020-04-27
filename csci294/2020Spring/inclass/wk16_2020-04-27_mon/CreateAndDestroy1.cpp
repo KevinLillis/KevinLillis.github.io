fa6d67bf05d7025338bdc06b409f54a8d5c010f6
Kevin Lillis <kmlillis30@hotmail.com>
2020-04-27 10:25:45 -0500

    294 daily files

---
+++ csci294/2020Spring/inclass/wk16_2020-04-27_mon/CreateAndDestroy1.cpp
@@ -0,0 +1,24 @@
+// File: CreateAndDestroy1.cpp
+# include <iostream>
+using namespace std;
+
+class CreateAndDestroy {
+	public:
+	CreateAndDestroy(){
+		cout << "Constructor called" << endl;
+	}
+	
+	~CreateAndDestroy(){
+		cout << "Destructor called" << endl;
+	}
+}; // End of class definition
+
+int main(int argc, char *argv[]){
+	CreateAndDestroy c_and_d;
+	cout << endl;
+	
+	CreateAndDestroy c_and_d2;
+	
+
+    return EXIT_SUCCESS;
+}
---
+++ csci294/2020Spring/inclass/wk16_2020-04-27_mon/args.cpp
@@ -0,0 +1,17 @@
+// File: args.cpp
+# include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+	cout << "argc: " << argc << endl;
+	cout << endl;
+	
+	for(int i{0}; i < argc; i++){
+		cout << "argv[" << i << "]: "
+		     <<  argv[i] << endl;
+	}
+	
+	cout << endl;
+	
+    return EXIT_SUCCESS;
+}
