64092bbc3b320aa74e2b02fd89fa3e8fc8c49ab2
Kevin Lillis <kmlillis30@hotmail.com>
2020-06-25 15:23:30 -0500

    hw 06 - hw 09

---
+++ cs1210/2020summer/assignments/hw08_chess-board/nestedLoop.py
@@ -0,0 +1,10 @@
+# nestedLoop.py
+
+def main():
+    for i in range(3):
+        for j in range(4):
+            print(f'i={i} and j={j}')
+            
+        print('------------')
+            
+main()
