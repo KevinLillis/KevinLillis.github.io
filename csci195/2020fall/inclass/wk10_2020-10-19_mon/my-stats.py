ea24d3e9d2f44360974d6a45e91293d9de2c037f
Kevin Lillis <kmlillis30@hotmail.com>
2020-10-21 07:10:06 -0500

    195 daily files from 10-19

---
+++ csci195/2020fall/inclass/wk10_2020-10-19_mon/my-stats.py
@@ -0,0 +1,103 @@
+# my-stats.py
+
+# ------------------------------------------------------------------
+# Dispersion (range)
+def getDispersion1(L):
+    return max(L) - min(L)
+
+def getDispersion2(L):
+    return getMax(L) - getMin(L)
+
+def getMax(L):
+    maxSoFar = L[0]
+    
+    for item in L:
+        if item > maxSoFar:
+            maxSoFar = item
+            
+    return maxSoFar
+
+def getMin(L):
+    minSoFar = L[0]
+    
+    for item in L:
+        if item < minSoFar:
+            minSoFar = item
+            
+    return minSoFar
+
+def getDispersion3(L):
+    maxSoFar = minSoFar = L[0]
+    for item in L:
+        if item > maxSoFar:
+            maxSoFar = item
+        if item < minSoFar:
+            minSoFar = item
+    
+    return maxSoFar - minSoFar
+
+# ------------------------------------------------------------------
+# Mean (average)
+
+def getMean1(L):
+    return sum(L) / len(L)
+
+def getMean2(L):
+    return getSum(L) / getLen(L)
+
+def getSum(L):
+    total = 0
+    for value in L:
+        total += value
+    return total
+
+def getLen(L):
+    length = 0
+    for value in L:
+        length += 1
+    return length
+
+# ------------------------------------------------------------------
+# Median (middle value)
+def getMedian(L):
+    # Make a sorted copy so we don't mess up L when we sort
+    L1 = sorted(L)
+    
+    if len(L1) % 2 == 1:
+        # Odd Length List
+        middleIndex = len(L1) // 2
+        median = L1[middleIndex]
+    else:
+        # Even Length List
+        middleLIndex = len(L1) // 2 - 1
+        middleRIndex = len(L1) // 2
+        
+        middleLValue = L1[middleLIndex]
+        middleRValue = L1[middleRIndex]
+        median = (middleLValue + middleRValue) / 2
+    
+    return median
+
+def main():
+    quakes = [20, 32, 21, 26, 33, 22, 18]
+    print('=' * 60)
+    dispersion1 = getDispersion1(quakes)
+    dispersion2 = getDispersion2(quakes)
+    dispersion3 = getDispersion3(quakes)
+    
+    print(f'Dispersion: {dispersion1}')
+    print(f'Dispersion: {dispersion2}')
+    print(f'Dispersion: {dispersion3}')
+    
+    print('=' * 60)
+    mean1 = getMean1(quakes)
+    mean2 = getMean2(quakes)
+    
+    print(f'Mean: {mean1}')
+    print(f'Mean: {mean2}')
+    
+    print('=' * 60)
+    median = getMedian(quakes)
+    print(f'Median: {median}')
+
+main()
